002c6616179c7f656b304925a835f32cef7ef489
jean-luc KRIKER <dahai0013@gmail.com>
2018-10-16 16:04:14 +0100

    version2
    2 version

---
+++ firstone.py
@@ -0,0 +1,10 @@
+from jnpr.junos import Device
+from pprint import pprint
+
+junos_hosts = [ '192.168.56.15' ]
+
+for ip in junos_hosts:
+    dev = Device(host=ip, user='lab', password='lab123')
+    dev.open()
+    ppr
+    dev.close()
